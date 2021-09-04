cb55386889ccb25efa27ae7faffaa3e4a6a73bb6
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-09-05 02:58:42 +0300

    one image use

--- client/src/pages/product-detail/ProductDetail.jsx
+++ client/src/pages/product-detail/ProductDetail.jsx
@@ -26,12 +26,6 @@ export default function ProductDetail() {
     {
       original: data.image,
     },
-    {
-      original: "https://picsum.photos/id/1018/1000/600/",
-    },
-    {
-      original: "https://picsum.photos/id/1015/1000/600/",
-    },
   ];
 
   return (
